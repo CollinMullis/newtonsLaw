0225b26a57ae70e39a90aae2f9437993b6e8ea46
Collin Mullis <cmullis@saintjoe.edu>
2015-04-21 11:23:54 -0400

    Added "add" Function

---
+++ newtonLaw.js
@@ -0,0 +1,16 @@
+// Variable for Surrounding "Room Temperature"
+
+var a =
+
+// Variable for Intial or Starting Temperature of Soup (0 Variable)
+
+var i = 
+
+// Constant that will be solved for
+
+var k = 
+
+// Variable for time
+
+var t = 
+var add = function(a, i)
